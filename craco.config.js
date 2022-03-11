dc7ef8345f03701d912fb4282b5bcc1b06c1963e
刘建伟 <liujianwei@liujianweideMacBook-Pro.local>
2022-03-11 21:08:44 +0800

    项目框架

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const resolve = (dir) => path.resolve(__dirname, dir);
+
+module.exports = {
+    webpack: {
+        alias: {
+            '@': resolve('src'),
+        },
+    },
+};
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React, { memo } from 'react';
+
+export default memo(function App() {
+    return <div>app</div>;
+});
