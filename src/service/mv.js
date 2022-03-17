0bc980b006c741d7bbfc7ee7b42876fb787097a6
hql362502 <2975999497@qq.com>
2022-03-17 13:37:14 +0800

    完成mv的推荐首页个性推荐的展示修改

--- src/service/mv.js
+++ src/service/mv.js
@@ -5,7 +5,7 @@ import request from './request'
 // order: 排序, 可选值为上升最快, 最热, 最新, 不填则为上升最快
 // limit: 取出数量, 默认为 30
 // offset: 偏移数量, 用于分页, 如 : (页数 - 1) * 50, 其中 50 为 limit 的值, 默认 为 0
-export function getAllMv(area, order, limit, offset) {
+export function getAllMv(area, order, limit=60, offset) {
     return request({
         url: '/mv/all',
         params: {
