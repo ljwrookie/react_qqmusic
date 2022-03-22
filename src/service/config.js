39702884196469b7c04c204824df9b1893600101
刘建伟 <liujianwei@liujianweideMacBook-Pro.local>
2022-03-22 23:10:24 +0800

    fix bugs

--- src/service/config.js
+++ src/service/config.js
@@ -1,8 +1,8 @@
 // 本地测试API
 // const devBaseURL = 'http://localhost:4000';
 // const proBaseURL = 'http://localhost:4000';
-const devBaseURL = 'http://47.109.27.210:3000';
-const proBaseURL = 'http://47.109.27.210:3000';
+const devBaseURL = 'http://47.108.191.181:3000';
+const proBaseURL = 'http://47.108.191.181:3000';
 
 export const BASE_URL =
     process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL;
